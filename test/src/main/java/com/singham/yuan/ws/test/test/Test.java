c921af4d42fd1364adeb8fb85bf8111b5fa65d78
singham.yuan <singham.yuan@derbysoft.com>
2018-03-07 18:37:57 +0800

    add new files

--- test/src/main/java/com/singham/yuan/ws/test/test/Test.java
+++ test/src/main/java/com/singham/yuan/ws/test/test/Test.java
@@ -6,6 +6,12 @@ public class Test {
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
+
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        System.out.println(loader);
+        System.out.println(loader.getParent());
+        System.out.println(loader.getParent().getParent());
     }
+
 
 }
